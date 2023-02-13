701f2d8967d6b30bfabbc4bd9144b1290f4e6717
Ivan Esin <ivan.esin.78@mail.ru>
2023-02-13 15:37:57 +0400

    Первое задание

---
+++ Project1/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Сколько чисел будет?");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+
+void main()
+{
+for (int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine("Введите число:");
+    int a = Convert.ToInt32(Console.ReadLine());
+    array [i] = a;
+}
+}
+
+void result()
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Чисел больше 0: {count}");
+}
+main();
+result();
